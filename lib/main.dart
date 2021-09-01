2caa46014a980d1c51458f119e45c05dbea6e71d
Amanda Cleto <amanda.clet@gmail.com>
2021-09-01 17:54:49 -0300

    created a section for image and name from user; created a section for todo list

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class App extends StatelessWidget {
       title: 'Flutter Teste', //icone da app
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: const Color(0xFFDCDCDD),
       ),
       home: HomePage(),
     );
@@ -22,10 +23,50 @@ class App extends StatelessWidget {
 class HomePage extends StatefulWidget {
   final List<Map<String, dynamic>> items = [];
   final List<Todo> todoList = [
-    Todo(description: 'Andar de Bike', checked: false),
-    Todo(description: 'Codar ThreeJs', checked: false),
-    Todo(description: 'Assistir Anime', checked: true),
-    Todo(description: 'Ouvir Música', checked: false),
+    Todo(
+      description: 'Andar de Bike',
+      icon: '57810',
+      complement: 'Ir até a Ufscar',
+      checked: false,
+    ),
+    Todo(
+      description: 'Codar ThreeJs',
+      icon: '57718',
+      checked: false,
+    ),
+    Todo(
+      description: 'Assistir Anime',
+      complement: 'Chihayafuru',
+      icon: '58267',
+      checked: true,
+    ),
+    Todo(
+      description: 'Ouvir Música',
+      icon: '58113',
+      checked: false,
+    ),
+    Todo(
+      description: 'Lição da Faculdade',
+      complement: 'Fazer o layout do App',
+      icon: '58713',
+      checked: false,
+    ),
+    Todo(
+      description: 'Dirigir',
+      icon: '61382',
+      checked: false,
+    ),
+    Todo(
+      description: 'Almoçar',
+      icon: '57946',
+      checked: false,
+    ),
+    Todo(
+      description: 'Arrumar o Github',
+      complement: 'Readme e Perfil',
+      icon: '57718',
+      checked: false,
+    ),
   ];
 
   @override
@@ -37,28 +78,149 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo do Caio :D'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('hello'),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text('hello'),
-              Text('hello'),
-              Text('hello'),
-              Container(
-                color: Colors.grey[200],
-                padding: EdgeInsets.all(20.0),
-                child: Text('derere'),
-              )
-            ],
+        title: Text(
+          'TodoList',
+          style: TextStyle(
+            fontFamily: 'RobotoSlab',
           ),
-        ],
+        ),
+        centerTitle: true,
+        backgroundColor: const Color(0xFF1985A1),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: new Border.all(
+                            color: Color(0xFF1985A1),
+                            width: 4.0,
+                          ),
+                        ),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/hero.jpeg'),
+                          radius: 40.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text(
+                        'Caio o Oliveira',
+                        style: TextStyle(
+                          color: Color(0xFF2E2E2E),
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.4,
+                          fontFamily: 'RobotoSlab',
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.email,
+                            color: Color(0xFF1985A1),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Text(
+                            'caioliveira@gmail.com',
+                            style: TextStyle(
+                              color: Color(0xFF1985A1),
+                              fontSize: 15.0,
+                              fontFamily: 'RobotoSlab',
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Expanded(
+              flex: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                ),
+                child: Container(
+                  color: Color(0xFF1985A1),
+                  child: ListView.builder(
+                    padding: EdgeInsets.fromLTRB(0, 20.0, 0, 0),
+                    itemCount: widget.todoList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final todo = widget.todoList[index];
+                      return Center(
+                        child: Container(
+                          child: Theme(
+                            child: CheckboxListTile(
+                              title: Text(
+                                todo.description,
+                                style: TextStyle(
+                                  color: Color(0xFF2E2E2E),
+                                  letterSpacing: 1.0,
+                                  fontFamily: 'RobotoSlab',
+                                ),
+                              ),
+                              subtitle: Text(
+                                todo.complement,
+                                style: TextStyle(
+                                  color: Color(0xFFDCDCDD),
+                                  fontFamily: 'RobotoSlab',
+                                ),
+                              ),
+                              secondary: Icon(
+                                IconData(
+                                  int.parse(todo.icon),
+                                  fontFamily: 'MaterialIcons',
+                                ),
+                                color: Color(0xFFC5C3C6),
+                                size: 25,
+                              ),
+                              activeColor: Color(0xFFC5C3C6),
+                              checkColor: Color(0xFF2E2E2E),
+                              key: Key(todo.description),
+                              value: todo.checked,
+                              onChanged: (value) {
+                                setState(() {
+                                  todo.checked = value;
+                                });
+                              },
+                            ),
+                            data: ThemeData(
+                                // checked color
+                                primarySwatch: Colors.pink,
+                                // border color
+                                unselectedWidgetColor: Color(0xFFC5C3C6)),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -66,33 +228,14 @@ class _HomePageState extends State<HomePage> {
 
 class Todo {
   String description;
+  String icon;
+  String complement;
   bool? checked;
 
-  Todo({this.description = '', this.checked = false});
+  Todo({
+    this.description = '',
+    this.icon = '',
+    this.complement = '',
+    this.checked = false,
+  });
 }
-
-
-// ListView.builder(
-//         itemCount: widget.todoList.length,
-//         itemBuilder: (BuildContext context, int index) {
-//           final todo = widget.todoList[index];
-//           return CheckboxListTile(
-//             title: Text(
-//               todo.description,
-//               style: TextStyle(
-//                 fontFamily: 'RobotoSlab',
-//                 color: Colors.lime[900],
-//                 fontWeight: FontWeight.w400,
-//                 fontSize: 16,
-//                 letterSpacing: 1.2,
-//               ),
-//             ),
-//             key: Key(todo.description),
-//             value: todo.checked,
-//             onChanged: (value) {
-//               setState(() {
-//                 todo.checked = value;
-//               });
-//             },
-//           );
-//         },
