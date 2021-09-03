44dbb731ac02b6fd0bea1dcfd0b3e92c17f5244a
Amanda Cleto <amanda.clet@gmail.com>
2021-09-03 18:03:33 -0300

    removed complement arg

--- lib/Todo.dart
+++ lib/Todo.dart
@@ -1,13 +1,11 @@
 class Todo {
   String description;
   String icon;
-  String complement;
   bool? checked;
 
   Todo({
     this.description = '',
     this.icon = '',
-    this.complement = '',
     this.checked = false,
   });
 }
