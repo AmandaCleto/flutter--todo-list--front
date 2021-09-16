7c641b6c6b3d6ca256065b3e7c13c5060817335a
Amanda Cleto <amanda.clet@gmail.com>
2021-09-16 18:01:06 -0300

    created dummy_todos

---
+++ lib/data/dummy_todos.dart
@@ -0,0 +1,15 @@
+import '../model/Todo.dart';
+
+// ignore: non_constant_identifier_names
+final dummy_todos = [
+  Todo(
+    createdTime: DateTime.now(),
+    title: 'Andar de Bike até a Ufscar',
+    isDone: true,
+  ),
+  Todo(
+    createdTime: DateTime.now(),
+    title: 'Almoçar',
+    isDone: true,
+  ),
+];
--- lib/provider/todos.dart
+++ lib/provider/todos.dart
@@ -1,31 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import '../model/Todo.dart';
 
-class TodosProvider extends ChangeNotifier {
-  List<Todo> _todos = [
-    Todo(
-      createdTime: DateTime.now(),
-      title: 'Andar de Bike até a Ufscar',
-      isDone: false,
-    ),
-    Todo(
-      createdTime: DateTime.now(),
-      title: 'Almoçar',
-      isDone: false,
-    ),
-    Todo(
-      createdTime: DateTime.now(),
-      title: 'Comprar óculos',
-      isDone: true,
-    ),
-    Todo(
-      createdTime: DateTime.now(),
-      title: 'Terminar SNK',
-      isDone: true,
-    ),
-  ];
+import '../data/dummy_todos.dart';
 
-  List<Todo> get todos => _todos.toList();
+class TodosProvider extends ChangeNotifier {
+  List<Todo> _todos = dummy_todos;
+
+  final id = Random().nextDouble().toString();
 
   List<Todo> get todosConcluded =>
       _todos.where((todo) => todo.isDone == true).toList();
